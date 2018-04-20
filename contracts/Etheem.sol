c4e44119e4a2594443f61c32d6a86da3469cc3a3
4000D <dkdkajej@gmail.com>
2018-04-21 05:19:15 +0900

    Etheem: init

---
+++ contracts/Etheem.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.18;
+
+import "@aragon/os/contracts/apm/APMRegistry.sol";
+import "@aragon/os/contracts/factory/DAOFactory.sol";
+import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/lib/minime/MiniMeToken.sol";
+import "./EtheemBase.sol";
+
+
+contract Etheem is AragonApp, EtheemBase {
+  
+}
